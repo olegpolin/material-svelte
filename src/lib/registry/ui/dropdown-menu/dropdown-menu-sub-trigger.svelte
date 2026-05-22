955207da294d638e382750f65214388389cda1da
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-05-22 10:28:33 -0400

    Fix dropdown menu item styles

--- src/lib/registry/ui/dropdown-menu/dropdown-menu-sub-trigger.svelte
+++ src/lib/registry/ui/dropdown-menu/dropdown-menu-sub-trigger.svelte
@@ -19,11 +19,11 @@
 	data-slot="dropdown-menu-sub-trigger"
 	data-inset={inset}
 	class={cn(
-		"data-highlighted:bg-accent data-highlighted:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:ps-8 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+		"data-highlighted:bg-foreground/8 data-highlighted:text-foreground data-[state=open]:bg-foreground/8 data-[state=open]:text-foreground [&_svg:not([class*='text-'])]:text-muted-foreground flex cursor-default items-center gap-3 px-4 py-3 min-h-12 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-40 data-[inset]:ps-10 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-5 transition-colors",
 		className
 	)}
 	{...restProps}
 >
 	{@render children?.()}
-	<ChevronRightIcon class="ms-auto size-4" />
+	<ChevronRightIcon class="ms-auto size-5" />
 </DropdownMenuPrimitive.SubTrigger>
