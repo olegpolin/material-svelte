474145a17a38dc344734905200b76fcee50d77b7
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-07-11 15:04:18 -0400

    Add siteUrl constant

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
-export const registryAddCommandPrefix =
-  'npx shadcn-svelte@latest add https://material-svelte.flenze.com/r/';
+export const siteUrl = 'https://material-svelte.flenze.com';
+
+export const registryAddCommandPrefix = `npx shadcn-svelte@latest add ${siteUrl}/r/`;
 
 export const githubRepoUrl = 'https://github.com/olegpolin/material-svelte';
